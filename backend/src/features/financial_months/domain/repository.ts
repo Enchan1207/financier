25beb3a7f54c4912aa3678a52c14c1198ea307ee
enchan <enchant0130@gmail.com>
2025-04-23 14:43:10 +0900

    feat: #6 リポジトリの基本的な実装

--- backend/src/features/financial_months/domain/repository.ts
+++ backend/src/features/financial_months/domain/repository.ts
@@ -4,9 +4,9 @@ import type { FinancialMonth, Months } from './entity'
 
 export interface FinancialMonthRepository {
   insertFinancialMonth(item: FinancialMonth): Promise<FinancialMonth>
+
   findByFinancialYear(financialYear: number): Promise<FinancialMonth[]>
   findByFinancialYearAndMonth(financialYear: number, month: Months):
   Promise<FinancialMonth | undefined>
   findByDate(date: dayjs.Dayjs): Promise<FinancialMonth | undefined>
-  findByDateRange(from: dayjs.Dayjs, to: dayjs.Dayjs): Promise<FinancialMonth[]>
 }
--- backend/src/features/financial_months/infrastructure/repositoryImpl.ts
+++ backend/src/features/financial_months/infrastructure/repositoryImpl.ts
@@ -1,24 +1,84 @@
+import dayjs from '@/logic/dayjs'
+import { condition, every } from '@/logic/queryBuilder/conditionTree'
+import { d1 } from '@/logic/queryBuilder/d1'
+
 import type { FinancialMonth, Months } from '../domain/entity'
 import type { FinancialMonthRepository } from '../domain/repository'
+import { FinancialMonthRecord } from './entity'
 
-const insertFinancialMonth = (db: D1Database): FinancialMonthRepository['insertFinancialMonth'] => async (props) => {
-  return props
+const makeFinancialMonth = (entity: FinancialMonthRecord): FinancialMonth => {
+  const { started_at, ended_at } = entity
+
+  return {
+    id: entity.id,
+    financialYear: entity.financial_year,
+    month: entity.month as Months,
+    startedAt: dayjs(started_at),
+    endedAt: dayjs(ended_at),
+  }
+}
+
+const insertFinancialMonth = (db: D1Database): FinancialMonthRepository['insertFinancialMonth'] => async (item) => {
+  const stmt = 'INSERT INTO financial_months VALUES (?1,?2,?3,?4,?5)'
+
+  await db.prepare(stmt).bind(
+    item.id,
+    item.financialYear,
+    item.month,
+    item.startedAt.valueOf(),
+    item.endedAt.valueOf(),
+  ).run()
+
+  return item
+}
+
+const findByFinancialYear = (db: D1Database): FinancialMonthRepository['findByFinancialYear'] => async (financialYear) => {
+  const stmt = d1(db)
+    .select(FinancialMonthRecord, 'financial_months')
+    .where(condition('financial_year', '==', financialYear))
+    .build()
+
+  const { results } = await stmt.run<FinancialMonthRecord>()
+  const items = results.map(makeFinancialMonth)
+
+  return items
+}
+
+const findByFinancialYearAndMonth = (db: D1Database): FinancialMonthRepository['findByFinancialYearAndMonth'] => async (financialYear, month) => {
+  const stmt = d1(db)
+    .select(FinancialMonthRecord, 'financial_months')
+    .where(every(
+      condition('financial_year', '==', financialYear),
+      condition('month', '==', month),
+    ))
+    .build()
+
+  const record = await stmt.first<FinancialMonthRecord>()
+  const item = record ? makeFinancialMonth(record) : undefined
+  return item
+}
+
+const findByDate = (db: D1Database): FinancialMonthRepository['findByDate'] => async (date) => {
+  const timestamp = date.valueOf()
+
+  const stmt = d1(db)
+    .select(FinancialMonthRecord, 'financial_months')
+    .where(every(
+      condition('started_at', '>=', timestamp),
+      condition('ended_at', '<=', timestamp),
+    ))
+    .build()
+
+  const record = await stmt.first<FinancialMonthRecord>()
+  const item = record ? makeFinancialMonth(record) : undefined
+  return item
 }
 
 export const useFinancialMonthRepositoryD1 = (db: D1Database): FinancialMonthRepository => {
   return {
     insertFinancialMonth: insertFinancialMonth(db),
-    findByFinancialYear: function (financialYear: number): Promise<FinancialMonth[]> {
-      throw new Error('Function not implemented.')
-    },
-    findByFinancialYearAndMonth: function (financialYear: number, month: Months): Promise<FinancialMonth | undefined> {
-      throw new Error('Function not implemented.')
-    },
-    findByDate: function (date: Dayjs): Promise<FinancialMonth | undefined> {
-      throw new Error('Function not implemented.')
-    },
-    findByDateRange: function (from: Dayjs, to: Dayjs): Promise<FinancialMonth[]> {
-      throw new Error('Function not implemented.')
-    },
+    findByFinancialYear: findByFinancialYear(db),
+    findByFinancialYearAndMonth: findByFinancialYearAndMonth(db),
+    findByDate: findByDate(db),
   }
 }
