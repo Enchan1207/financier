27b7e9f66d9f7cf26fe957f3a37dd7775c4876ca
enchan <enchant0130@gmail.com>
2025-04-27 12:05:45 +0900

    test: #8 詳細検索のテストケースを構成

--- backend/src/features/definitions/income/infrastructure/repositoryImpl.test.ts
+++ backend/src/features/definitions/income/infrastructure/repositoryImpl.test.ts
@@ -1,10 +1,21 @@
 import { env } from 'cloudflare:test'
 
+import type { Months } from '@/features/financial_months/domain/valueObject'
 import { getPeriodByFinancialMonth } from '@/features/financial_months/domain/valueObject'
 
 import type { IncomeDefinition } from '../domain/entity'
 import { createIncomeDefinition } from '../domain/entity'
 import { useIncomeDefinitionRepositoryD1 } from './repositoryImpl'
+
+/** エンティティを相互変換可能な型に変換する */
+const makeComparable = (entity?: IncomeDefinition) => {
+  return {
+    ...entity,
+    enabledAt: entity?.enabledAt.valueOf(),
+    disabledAt: entity?.disabledAt.valueOf(),
+    updatedAt: entity?.updatedAt.valueOf(),
+  }
+}
 
 describe('基本的なCRUD', () => {
   const repository = useIncomeDefinitionRepositoryD1(env.D1)
@@ -38,17 +49,8 @@ describe('基本的なCRUD', () => {
     })
 
     test('同じ項目を取得できること', () => {
-      expect({
-        ...actual,
-        enabledAt: actual?.enabledAt.valueOf(),
-        disabledAt: actual?.disabledAt.valueOf(),
-        updatedAt: actual?.updatedAt.valueOf(),
-      }).toStrictEqual({
-        ...dummyDefinition,
-        enabledAt: dummyDefinition.enabledAt.valueOf(),
-        disabledAt: dummyDefinition.disabledAt.valueOf(),
-        updatedAt: dummyDefinition.updatedAt.valueOf(),
-      })
+      expect(makeComparable(actual))
+        .toStrictEqual(makeComparable(dummyDefinition))
     })
   })
 
@@ -82,12 +84,7 @@ describe('基本的なCRUD', () => {
         month: 1,
       })
 
-      expect({
-        ...actual,
-        enabledAt: actual?.enabledAt.valueOf(),
-        disabledAt: actual?.disabledAt.valueOf(),
-        updatedAt: actual?.updatedAt.valueOf(),
-      }).toStrictEqual({
+      expect(makeComparable(actual)).toStrictEqual({
         id: dummyDefinition.id,
         userId: dummyDefinition.userId,
         kind: 'related_by_workday',
@@ -109,5 +106,88 @@ describe('基本的なCRUD', () => {
       const actual = await repository.findById(dummyDefinition.id)
       expect(actual).toBeUndefined()
     })
+  })
+})
+
+describe('詳細な検索', () => {
+  const repository = useIncomeDefinitionRepositoryD1(env.D1)
+
+  const userId = 'test_user'
+
+  const dummyDefinition1 = createIncomeDefinition({
+    userId,
+    name: '手当1',
+    kind: 'related_by_workday',
+    value: 380,
+    from: {
+      financialYear: 2025,
+      month: 4,
+    },
+    to: {
+      financialYear: 2025,
+      month: 12,
+    },
+  })
+
+  const dummyDefinition2 = createIncomeDefinition({
+    userId,
+    name: '手当2',
+    kind: 'related_by_workday',
+    value: 380,
+    from: {
+      financialYear: 2025,
+      month: 8,
+    },
+    to: {
+      financialYear: 2025,
+      month: 2,
+    },
+  })
+
+  beforeAll(async () => {
+    await repository.insertIncomeDefinition(dummyDefinition1)
+    await repository.insertIncomeDefinition(dummyDefinition2)
+  })
+
+  test.each([
+    {
+      month: 4,
+      expected: [dummyDefinition1],
+    },
+    {
+      month: 8,
+      expected: [dummyDefinition1, dummyDefinition2],
+    },
+    {
+      month: 1,
+      expected: [dummyDefinition2],
+    },
+  ])('単月検索 $month月度', async ({ month, expected }) => {
+    const actual = await repository.findByFinancialMonth({
+      userId,
+      financialMonth: {
+        financialYear: 2025,
+        month: month as Months,
+      },
+      sortBy: 'enabledAt',
+      order: 'asc',
+      limit: 100,
+    })
+
+    expect(actual.map(makeComparable))
+      .toStrictEqual(expected.map(makeComparable))
+  })
+
+  test('年度全体での検索', async () => {
+    const actual = await repository.findByFinancialYear({
+      userId,
+      financialYear: 2025,
+      sortBy: 'enabledAt',
+      order: 'asc',
+      limit: 100,
+    })
+
+    expect(actual.map(makeComparable))
+      .toStrictEqual([dummyDefinition1, dummyDefinition2].map(makeComparable))
   })
 })
