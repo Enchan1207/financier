736f7d38490aca195f7503cebbff64bbbd55d25b
enchan <enchant0130@gmail.com>
2025-04-27 15:32:34 +0900

    feat: #20 報酬実績の基本的なエンティティ定義を実装

---
+++ backend/src/features/records/income/infrastructure/entity.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+
+import { IncomeRecordUpdator } from '../domain/entity'
+
+// 草
+export const IncomeRecordRecord = z.object({
+  id: z.string(),
+  user_id: z.string(),
+
+  financial_month_id: z.string(),
+  definition_id: z.string(),
+
+  value: z.number(),
+
+  updated_at: z.number(),
+  updated_by: z.enum(IncomeRecordUpdator),
+})
+
+export type IncomeRecordRecord = z.infer<typeof IncomeRecordRecord>
