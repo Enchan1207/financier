7454915bb267f03e3fe1923af65dd0445d879200
enchan <enchant0130@gmail.com>
2025-05-31 21:15:22 +0900

    test: #52 ワークフローのテストケースを実装

---
+++ backend/src/features/standard_income/workflow/get.test.ts
@@ -0,0 +1,80 @@
+import { env } from 'cloudflare:test'
+
+import { createStandardIncomeGrade, createStandardIncomeTable } from '@/domains/standard_income/logic'
+import type { User } from '@/domains/user'
+import { createUser } from '@/domains/user/logic'
+import { saveUser } from '@/features/authorize/dao'
+import { EntityNotFoundError } from '@/logic/errors'
+
+import { getStandardIncomeTable, insertStandardIncomeTable } from '../dao'
+import { createStandardIncomeTableGetWorkflow } from './get'
+
+describe('標準報酬月額表取得ワークフロー', () => {
+  const dummyUser: User = createUser({
+    name: 'testuser',
+    email: 'test@example.com',
+    auth0UserId: 'auth0_test_user',
+  })
+
+  const anotherUser: User = createUser({
+    name: 'another',
+    email: 'another@example.com',
+    auth0UserId: 'auth0_another_user',
+  })
+
+  const dummyEntity = createStandardIncomeTable({
+    userId: dummyUser.id,
+    name: 'test table',
+    grades: [
+      {
+        threshold: 100000,
+        standardIncome: 100000,
+      },
+      {
+        threshold: 200000,
+        standardIncome: 200000,
+      },
+    ].map(grade => createStandardIncomeGrade(grade)._unsafeUnwrap()),
+  })._unsafeUnwrap()
+
+  const workflow = createStandardIncomeTableGetWorkflow({
+    //
+    getStandardIncomeTable: getStandardIncomeTable(env.D1),
+  })
+
+  beforeAll(async () => {
+    await saveUser(env.D1)(dummyUser)
+    await saveUser(env.D1)(anotherUser)
+    await insertStandardIncomeTable(env.D1)(dummyEntity)
+  })
+
+  test('存在しない項目は取得できないこと', async () => {
+    const command = {
+      input: { id: 'invalid_id' },
+      state: { user: dummyUser },
+    }
+
+    const result = await workflow(command)
+    expect(result._unsafeUnwrapErr()).toBeInstanceOf(EntityNotFoundError)
+  })
+
+  test('他人の項目は取得できないこと', async () => {
+    const command = {
+      input: { id: dummyEntity.id },
+      state: { user: anotherUser },
+    }
+
+    const result = await workflow(command)
+    expect(result._unsafeUnwrapErr()).toBeInstanceOf(EntityNotFoundError)
+  })
+
+  test('自分の項目を取得できること', async () => {
+    const command = {
+      input: { id: dummyEntity.id },
+      state: { user: dummyUser },
+    }
+
+    const actual = (await workflow(command))._unsafeUnwrap()
+    expect(actual).toStrictEqual(dummyEntity)
+  })
+})
---
+++ backend/src/features/standard_income/workflow/list.test.ts
@@ -0,0 +1,112 @@
+import { env } from 'cloudflare:test'
+
+import { createStandardIncomeGrade, createStandardIncomeTable } from '@/domains/standard_income/logic'
+import type { User } from '@/domains/user'
+import { createUser } from '@/domains/user/logic'
+import { saveUser } from '@/features/authorize/dao'
+
+import { insertStandardIncomeTable, listStandardIncomeTables } from '../dao'
+import { createStandardIncomeTablesListWorkflow } from './list'
+
+describe('標準報酬月額表一覧取得ワークフロー', () => {
+  const dummyUser: User = createUser({
+    name: 'testuser',
+    email: 'test@example.com',
+    auth0UserId: 'auth0_test_user',
+  })
+
+  const anotherUser: User = createUser({
+    name: 'another',
+    email: 'another@example.com',
+    auth0UserId: 'auth0_another_user',
+  })
+
+  // 2つのダミーエンティティを用意
+  const dummyTable1 = createStandardIncomeTable({
+    userId: dummyUser.id,
+    name: 'test table 1',
+    grades: [
+      {
+        threshold: 100000,
+        standardIncome: 100000,
+      },
+    ].map(grade => createStandardIncomeGrade(grade)._unsafeUnwrap()),
+  })._unsafeUnwrap()
+
+  const dummyTable2 = createStandardIncomeTable({
+    userId: dummyUser.id,
+    name: 'test table 2',
+    grades: [
+      {
+        threshold: 200000,
+        standardIncome: 200000,
+      },
+    ].map(grade => createStandardIncomeGrade(grade)._unsafeUnwrap()),
+  })._unsafeUnwrap()
+
+  const workflow = createStandardIncomeTablesListWorkflow({
+    //
+    listStandardIncomeTables: listStandardIncomeTables(env.D1),
+  })
+
+  beforeAll(async () => {
+    await saveUser(env.D1)(dummyUser)
+    await insertStandardIncomeTable(env.D1)(dummyTable1)
+    await insertStandardIncomeTable(env.D1)(dummyTable2)
+  })
+
+  describe('自分の項目の取得', () => {
+    test('自分の項目の件数が正しいこと', async () => {
+      const command = {
+        input: { order: 'asc' as const },
+        state: { user: dummyUser },
+      }
+
+      const result = await workflow(command)
+      expect(result.isOk()).toBeTruthy()
+
+      const tables = result._unsafeUnwrap()
+      expect(tables).toHaveLength(2)
+    })
+
+    test('昇順で取得した場合、正しい順序で項目が取得できること', async () => {
+      const command = {
+        input: { order: 'asc' as const },
+        state: { user: dummyUser },
+      }
+
+      const result = await workflow(command)
+      const tables = result._unsafeUnwrap()
+
+      expect(tables[0].name).toBe(dummyTable1.name)
+      expect(tables[1].name).toBe(dummyTable2.name)
+    })
+  })
+
+  test('他人の項目は取得されないこと', async () => {
+    const command = {
+      input: { order: 'asc' as const },
+      state: { user: anotherUser },
+    }
+
+    const result = await workflow(command)
+    expect(result.isOk()).toBeTruthy()
+
+    const tables = result._unsafeUnwrap()
+    expect(tables).toHaveLength(0)
+  })
+
+  test('降順で取得した場合、正しい順序で項目が取得できること', async () => {
+    const command = {
+      input: { order: 'desc' as const },
+      state: { user: dummyUser },
+    }
+
+    const result = await workflow(command)
+    expect(result.isOk()).toBeTruthy()
+
+    const tables = result._unsafeUnwrap()
+    expect(tables[0].name).toBe(dummyTable2.name)
+    expect(tables[1].name).toBe(dummyTable1.name)
+  })
+})
