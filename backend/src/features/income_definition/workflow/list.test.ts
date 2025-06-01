0e55be68a1187ba3c045c870b3c861f749059aa6
enchan <enchant0130@gmail.com>
2025-06-01 13:02:51 +0900

    test: ワークフローのテストを修正

--- backend/src/features/income_definition/workflow/list.test.ts
+++ backend/src/features/income_definition/workflow/list.test.ts
@@ -1,11 +1,11 @@
 import { env } from 'cloudflare:test'
 
-import type { FinancialMonthData } from '@/domains/financial_month'
-import { createFinancialMonthData } from '@/domains/financial_month/logic'
+import { createFinancialYear } from '@/domains/financial_year/logic'
 import { createIncomeDefinition } from '@/domains/income_definition/logic'
 import type { User } from '@/domains/user'
 import { createUser } from '@/domains/user/logic'
 import { saveUser } from '@/features/authorize/dao'
+import { insertFinancialYear } from '@/features/financial_year/dao'
 
 import { findIncomeDefinitions, insertIncomeDefinition } from '../dao'
 import type { UnvalidatedListIncomeDefinitionCommand } from './list'
@@ -24,17 +24,22 @@ describe('報酬定義一覧取得ワークフロー', () => {
     auth0UserId: 'auth0_another_user',
   })
 
-  const fromMonth1: FinancialMonthData = createFinancialMonthData({
-    financialYear: 2025,
-    month: 4,
-    workday: 20,
+  const dummyFinancialYear = createFinancialYear({
+    userId: dummyUser.id,
+    year: 2025,
   })._unsafeUnwrap()
 
-  const toMonth1: FinancialMonthData = createFinancialMonthData({
-    financialYear: 2025,
-    month: 12,
-    workday: 20,
-  })._unsafeUnwrap()
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const dummyApril = dummyFinancialYear.months.find(({ month }) => month === 4)!
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const dummyAugust = dummyFinancialYear.months.find(({ month }) => month === 8)!
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const dummySeptember = dummyFinancialYear.months.find(({ month }) => month === 9)!
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const dummyFebruary = dummyFinancialYear.months.find(({ month }) => month === 2)!
 
   const dummyDefinition1 = createIncomeDefinition({
     userId: dummyUser.id,
@@ -42,20 +47,8 @@ describe('報酬定義一覧取得ワークフロー', () => {
     kind: 'related_by_workday',
     value: 350,
     isTaxable: true,
-    from: fromMonth1,
-    to: toMonth1,
-  })._unsafeUnwrap()
-
-  const fromMonth2: FinancialMonthData = createFinancialMonthData({
-    financialYear: 2025,
-    month: 8,
-    workday: 20,
-  })._unsafeUnwrap()
-
-  const toMonth2: FinancialMonthData = createFinancialMonthData({
-    financialYear: 2026,
-    month: 2,
-    workday: 20,
+    from: dummyApril,
+    to: dummySeptember,
   })._unsafeUnwrap()
 
   const dummyDefinition2 = createIncomeDefinition({
@@ -64,8 +57,8 @@ describe('報酬定義一覧取得ワークフロー', () => {
     kind: 'absolute',
     value: 15000,
     isTaxable: false,
-    from: fromMonth2,
-    to: toMonth2,
+    from: dummyAugust,
+    to: dummyFebruary,
   })._unsafeUnwrap()
 
   const workflow = createIncomeDefinitionListWorkflow({
@@ -76,6 +69,7 @@ describe('報酬定義一覧取得ワークフロー', () => {
   beforeAll(async () => {
     await saveUser(env.D1)(dummyUser)
     await saveUser(env.D1)(anotherUser)
+    await insertFinancialYear(env.D1)(dummyFinancialYear)
     await insertIncomeDefinition(env.D1)(dummyDefinition1)
     await insertIncomeDefinition(env.D1)(dummyDefinition2)
   })
@@ -86,6 +80,7 @@ describe('報酬定義一覧取得ワークフロー', () => {
         input: {
           sortBy: 'enabledAt',
           from: '2025_04',
+          to: '2025_03',
           order: 'asc',
           limit: 100,
         },
@@ -102,6 +97,8 @@ describe('報酬定義一覧取得ワークフロー', () => {
           sortBy: 'enabledAt',
           order: 'asc',
           limit: 100,
+          from: '2025_04',
+          to: '2025_03',
         },
         state: { user: anotherUser },
       }
@@ -118,6 +115,8 @@ describe('報酬定義一覧取得ワークフロー', () => {
         order: 'asc',
         limit: 100,
         kind: 'absolute',
+        from: '2025_04',
+        to: '2025_03',
       },
       state: { user: dummyUser },
     }
@@ -127,49 +126,63 @@ describe('報酬定義一覧取得ワークフロー', () => {
     expect(actual[0].id).toBe(dummyDefinition2.id)
   })
 
-  test('開始期間と終了期間を指定してフィルタリングできること', async () => {
+  test.each([
+    {
+      from: '2025_04',
+      to: '2025_07',
+      at: undefined,
+      expected: [dummyDefinition1.id],
+    },
+    {
+      from: '2025_04',
+      to: '2025_08',
+      at: undefined,
+      expected: [dummyDefinition1.id, dummyDefinition2.id],
+    },
+    {
+      from: '2025_08',
+      to: '2025_09',
+      at: undefined,
+      expected: [dummyDefinition1.id, dummyDefinition2.id],
+    },
+    {
+      from: '2025_09',
+      to: '2025_10',
+      at: undefined,
+      expected: [dummyDefinition1.id, dummyDefinition2.id],
+    },
+    {
+      from: '2025_10',
+      to: '2025_02',
+      at: undefined,
+      expected: [dummyDefinition2.id],
+    },
+    {
+      at: '2025_08',
+      expected: [dummyDefinition1.id, dummyDefinition2.id],
+    },
+    {
+      at: '2025_09',
+      expected: [dummyDefinition1.id, dummyDefinition2.id],
+    },
+  ])('from: $from, to: $to, at: $at でフィルタした際 $expected.length件の定義が得られること', async ({
+    from, to, at, expected,
+  }) => {
     const command: UnvalidatedListIncomeDefinitionCommand = {
       input: {
         sortBy: 'enabledAt',
         order: 'asc',
         limit: 100,
-        from: '2025_04',
-        to: '2025_07',
+        from,
+        to,
+        at,
       },
       state: { user: dummyUser },
     }
 
     const actual = (await workflow(command))._unsafeUnwrap()
 
-    expect(actual).toHaveLength(1)
-    expect(actual[0].id).toBe(dummyDefinition1.id)
-  })
-
-  test('特定時点で有効な定義をフィルタリングできること', async () => {
-    const command: UnvalidatedListIncomeDefinitionCommand = {
-      input: {
-        sortBy: 'enabledAt',
-        order: 'asc',
-        limit: 100,
-        at: '2025_10',
-      },
-      state: { user: dummyUser },
-    }
-
-    const actual = (await workflow(command))._unsafeUnwrap()
-    expect(actual).toHaveLength(2)
-  })
-  test('更新日時の降順で取得できること', async () => {
-    const command: UnvalidatedListIncomeDefinitionCommand = {
-      input: {
-        sortBy: 'updatedAt',
-        order: 'desc',
-        limit: 100,
-      },
-      state: { user: dummyUser },
-    }
-
-    const actual = (await workflow(command))._unsafeUnwrap()
-    expect(actual[0].updatedAt.isAfter(actual[1].updatedAt)).toBeTruthy()
+    const ids = actual.map(def => def.id)
+    expect(ids).toStrictEqual(expected)
   })
 })
