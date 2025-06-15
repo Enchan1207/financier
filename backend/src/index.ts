7f7053188e25a012009dffbfe8a7cc9136927fbf
enchan <enchant0130@gmail.com>
2025-06-15 13:46:12 +0900

    feat: #83 各機能のルーティングをルートのappインスタンスにつないだ

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
 
+import financialYears from '@/features/financial_year/route'
+import incomeDefinitions from '@/features/income_definition/route'
 import users from '@/features/profile/route'
+import standardIncomes from '@/features/standard_income/route'
 import { corsMiddleware } from '@/logic/middlewares/cors'
 
-const app = new Hono().use(corsMiddleware).route('/user', users)
+const app = new Hono()
+  .use(corsMiddleware)
+  .route('/users', users)
+  .route('/financial_years', financialYears)
+  .route('/income_definitions', incomeDefinitions)
+  .route('/standard_incomes', standardIncomes)
 
 export default app
 export type AppType = typeof app
--- frontend/src/repositories/userRepository.ts
+++ frontend/src/repositories/userRepository.ts
@@ -10,7 +10,7 @@ export const getCurrentUser = async (
   token: string,
 ): Promise<Result<User, NetworkError>> => {
   try {
-    const response = await client.user.me.$get(
+    const response = await client.users.me.$get(
       {},
       {
         headers: { Authorization: `Bearer ${token}` },
