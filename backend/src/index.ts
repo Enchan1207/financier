ecc1247adbdfa1089ce60fe6f82913b131293756
enchan <enchant0130@gmail.com>
2025-06-22 16:00:40 +0900

    chore: #88 レガシ機能を排除

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 
-import financialYears from '@/features/financial_year/route'
 import users from '@/features/profile/route'
 import standardIncomes from '@/features/standard_income/route'
 import { corsMiddleware } from '@/logic/middlewares/cors'
@@ -8,7 +7,6 @@ import { corsMiddleware } from '@/logic/middlewares/cors'
 const app = new Hono()
   .use(corsMiddleware)
   .route('/users', users)
-  .route('/financial_years', financialYears)
   .route('/standard_incomes', standardIncomes)
 
 export default app
