71c3c71de24bfd4fff3601e05ac4d39b2cf50442
enchan <enchant0130@gmail.com>
2025-06-22 16:34:00 +0900

    feat: #92 会計年度エンティティ生成ロジックを追加

--- backend/src/domains/financial_year/logic.ts
+++ backend/src/domains/financial_year/logic.ts
@@ -1,39 +1,24 @@
-import { Result } from 'neverthrow'
+import type { Result } from 'neverthrow'
+import { ulid } from 'ulid'
 
-import { ValidationError } from '@/logic/errors'
+import type { ValidationError } from '@/logic/errors'
 import { parseSchema } from '@/logic/zod'
 
 import { Months } from '../monthly_context'
-import { createMonthlyContext } from '../monthly_context/logic'
 import type { StandardIncomeTable } from '../standard_income'
 import type { User } from '../user'
 import type { FinancialYear } from '.'
-import { FinancialYearValueSchema } from '.'
-
-export const createFinancialYearValue = (value: number) =>
-  parseSchema(FinancialYearValueSchema, value).mapErr(
-    (error) => new ValidationError(error.message),
-  )
+import { FinancialYearSchema } from '.'
 
 export const createFinancialYear = (props: {
   userId: User['id']
-  financialYear: number
-  standardIncomeTableId: StandardIncomeTable['id']
-}): Result<FinancialYear, ValidationError> => {
-  const { userId, financialYear, standardIncomeTableId } = props
-
-  const results = Months.map((month) =>
-    createMonthlyContext({
-      userId,
-      financialYear,
-      month,
-      workday: 20, // TODO: 本来は各月の祝日を参照するべき
-      standardIncomeTableId,
-    }),
-  )
-
-  return Result.combine(results).map((months) => ({
-    year: months[0].financialYear,
-    months,
-  }))
-}
+  year: number
+  standardIncomeTable: StandardIncomeTable
+}): Result<FinancialYear, ValidationError> =>
+  parseSchema(FinancialYearSchema, {
+    id: ulid(),
+    userId: props.userId,
+    year: props.year,
+    months: Months.map((month) => ({ id: ulid(), month })),
+    standardIncomeTable: props.standardIncomeTable,
+  })
